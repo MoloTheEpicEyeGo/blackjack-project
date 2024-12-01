781ad062b283ca07cd09eb36fecf5bfe4f5cdf43
travis <molotheepiceyego@gmail.com>
2024-11-30 22:58:30 -0800

    added a method that calculates the numerical value of either the player or dealers current hand.

--- src/util.java
+++ src/util.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class util
 {
@@ -25,5 +26,38 @@ public class util
                 System.out.println("Invalid input. Please enter 'y' for yes or 'n' for no.");
             }
         }
+    }
+
+    public static int calculateHand(ArrayList<String> hand)
+    {
+        //aceCount tracks Aces to adjust their value from 11 to 1 if needed
+        int aceCount = 0;
+        int handVal = 0;
+
+        for (String card : hand)
+        {
+            char rank = card.charAt(1);
+
+            if (rank == 'A')
+            {
+                aceCount++;
+                handVal += 11;
+            }
+            else if (rank == 'K' || rank == 'Q' || rank == 'J' || rank == '1')
+            {
+                handVal += 10;
+            }
+            else
+            {
+                handVal += Character.getNumericValue(rank); //changes char into an int
+            }
+        }
+
+        while (handVal > 21 && aceCount > 0)
+        {
+            handVal -= 10; //changes 11 to 1 to adjust score.
+            aceCount--;
+        }
+        return handVal;
     }
 }
