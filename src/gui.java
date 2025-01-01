96695cadefc6ce34e3abafd22900ce824431c49f
molo2 <molotheepiceyego@gmail.com>
2025-01-01 02:57:49 -0800

    - added alot of stuff from source to gui - hit button outputs a card but doesnt calculate correctly and if statements are useless - draws dealers cards - draws players cards - gui will refresh after every hit

--- src/gui.java
+++ src/gui.java
@@ -1,30 +1,37 @@
 //imports
 
-import javax.smartcardio.Card;
 import javax.swing.*;
-import javax.swing.border.Border;
 import java.awt.*;
-import java.util.ArrayList;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class gui
 {
     //window dimensions
-    int width = 600;
+    int width = 1200;
     int length = 600;
     int cardWidth = 110;
     int cardHeight = 154;
 
-
-    //dealer&player objects&vars
-    dealer dealer = new dealer();
-
+    //player and dealer data
+    private dealer dealer;
+    private player player;
+    private util util;
 
 
     //gui objects
+
+    //main frame
     JFrame frame = new JFrame("BlackJack 1.0");
+
+    //frame for buttons
     JPanel buttonPanel = new JPanel();
+
+    //individual buttons
     JButton hitButton = new JButton("hit");
-    JButton stayButton = new JButton("stay");
+    JButton stayButton = new JButton("stand");
+
+    //panel that draws cards
     JPanel panel = new JPanel()
     {
         @Override
@@ -34,21 +41,35 @@ public class gui
 
             try
             {
-                //drawing hidden card
+                //draw first card from dealers hand
+                String card = "./cards/" + dealer.getHand(0) + ".png";
+                Image cardImg = new ImageIcon(getClass().getResource(card)).getImage();
+                g.drawImage(cardImg, 20, 20, cardWidth, cardHeight, null);
+
+                //drawing hidden card to cover the first card of the dealers hand
                 Image hiddenCardImg = new ImageIcon(getClass().getResource("./cards/BACK.png")).getImage();
                 g.drawImage(hiddenCardImg, 20, 20, cardWidth, cardHeight, null);
 
-                //drawing dealers hand
-                /*
-                    have to change whole card array to match card files so it can get correct name. Thats why its not printing
-                */
-                for (int i = 0; i < dealer.getHand().size(); i++)
-                {
-                    String card = dealer.getHand(i);
-                    Image cardImg = new ImageIcon(getClass().getResource("./cards/" + card + ".png")).getImage();
-                    g.drawImage(cardImg, 20 + (i * cardWidth), 200, cardWidth, cardHeight, null);
 
+
+                //drawing dealers hand
+                for (int i = 1; i < dealer.getHand().size(); i++) //size should be 2
+                {
+
+                    String card1 = "./cards/" + dealer.getHand(i) + ".png";
+                    Image cardImg1 = new ImageIcon(getClass().getResource(card1)).getImage();
+                    g.drawImage(cardImg1,140 * i, 20, cardWidth, cardHeight, null);
                 }
+
+                //drawing players hand
+                for (int i = 0; i < player.getHand().size(); i++) //size should be 2
+                {
+
+                    String card1 = "./cards/" + player.getHand(i) + ".png";
+                    Image cardImg1 = new ImageIcon(getClass().getResource(card1)).getImage();
+                    g.drawImage(cardImg1, 20 + (cardWidth + 5) * i ,320, cardWidth, cardHeight, null);
+                }
+
             } catch(Exception e)
             {
                 e.printStackTrace();
@@ -57,11 +78,12 @@ public class gui
     };//background color & drawing cards by overRiding "paintComponents" method to output image of cards
 
 
-
-
-
-    public gui(dealer dealer)
+    public gui(dealer dealer, player player, Cards deck)
     {
+        //storing the dealers data from the main class
+        this.dealer = dealer;
+        this.player = player;
+
         frame.setSize(width, length);
         frame.setVisible(true); //makes the window visable
         frame.setLocationRelativeTo(null); //will set it to the middle of the screen
@@ -74,11 +96,30 @@ public class gui
         frame.add(panel);//adds to the main frame
 
         //hit button
-        hitButton.setPreferredSize(new Dimension(100, 100)); //changes the size of the button
+        hitButton.setPreferredSize(new Dimension(100, 50)); //changes the size of the button
         hitButton.setFocusable(false); //makes it unhighlightable
+        hitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String card = deck.drawCard();
+                player.hand.add(card);
+                int score = util.calculateHand(player.hand);
+
+                if (util.calculateHand(player.getHand()) > util.calculateHand(dealer.getHand()) && !player.getBust()) {
+                    System.out.println("player WINS!");
+                } else if (util.calculateHand(player.getHand()) < util.calculateHand(dealer.getHand()) && !dealer.getBust()) {
+                    System.out.println("player LOSES!");
+
+                } else if (util.calculateHand(player.getHand()) == util.calculateHand(dealer.getHand()) && !player.getBust() && !dealer.getBust()) {
+                    System.out.println("player PUSHES!");
+
+                }
+                panel.repaint();
+            }
+        });
 
         //stay button
-        stayButton.setPreferredSize(new Dimension(100, 100));
+        stayButton.setPreferredSize(new Dimension(100, 50));
         stayButton.setFocusable(false);
 
         buttonPanel.add(hitButton); //adding stay & hit buttons to the button panel
@@ -86,6 +127,7 @@ public class gui
 
 
         frame.add(buttonPanel, BorderLayout.SOUTH); //adding the buttonPanel (stores hit & stay button) into frame
+        panel.repaint();
     }
 
 
